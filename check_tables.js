2dcec13e0b52fb327fde1ebb270a1ead0bbcc274
Okta Pramtama <oktaiwp849@gmail.com>
2026-06-30 22:43:51 +0700

    delete testing item

--- check_tables.js
+++
@@ -1,2 +0,0 @@
-const db = require('./db');
-db.query("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'daily_sales'").then(r => console.log(r.rows)).catch(console.error).finally(()=>process.exit(0));
--- migrate.js
+++
@@ -1,5 +0,0 @@
-const pool=require('./db.js');
-pool.query("ALTER TABLE salary_logs ADD COLUMN base_salary_applied NUMERIC;")
-  .then(() => pool.query("UPDATE salary_logs SET base_salary_applied = CASE WHEN is_off THEN 0 ELSE amount + COALESCE(deduction,0) - COALESCE(bonus,0) END"))
-  .then(() => { console.log('Migrated'); process.exit(); })
-  .catch(e => { console.error(e); process.exit(); });
